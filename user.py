8130bf3c6fde1e9f0fb6cf1efbac841d9f48b3a2
charles K <kcharles52@gmail.com>
2018-07-30 12:00:05 +0300

    Create user class

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    users =[]
+
+    def __init__(self,username,password):
+        """Initialise class instances """
+        self.username = username
+        self.password = password
+
+    def signup(self):
+        User.users.append(self)
